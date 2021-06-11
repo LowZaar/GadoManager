2a3069e2e6cd89cb9b79ad5f6351e8df94a04c95
Low Zaar <watchmefail12@gmail.com>
2021-06-11 03:27:23 -0300

    fix FXML classpath masks added to cadastroEmpresaController

--- module-info.java
+++ module-info.java
@@ -1,13 +1,12 @@
 module gadoManager {
 
+	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires java.sql;
 	requires org.controlsfx.controls;
 	requires java.persistence;
-	
 	opens gadoManager to javafx.graphics, javafx.controls;
-	
 	exports gadoManager;
 }
--- src/main/java/controllers/cadastroEmpresaController.java
+++ src/main/java/controllers/cadastroEmpresaController.java
@@ -6,7 +6,6 @@ import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
-
 import classes.Cidades;
 import classes.Empresas_Pessoas;
 import classes.Estados;
@@ -26,6 +25,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import utils.DAOHibernate;
+import utils.TextFieldFormatter;
 
 public class cadastroEmpresaController {
 
@@ -49,9 +49,6 @@ public class cadastroEmpresaController {
 
 	@FXML
 	private TextField txtCNPJ;
-
-	@FXML
-	private TextField txtCPF;
 
 	@FXML
 	private DatePicker dateDataNascimento;
@@ -101,9 +98,7 @@ public class cadastroEmpresaController {
 	@FXML
 	private PasswordField passSenha;
 
-	public cadastroEmpresaController() {
 
-	}
 
 	@FXML
 	public void Salvar() throws Exception {
@@ -178,7 +173,7 @@ public class cadastroEmpresaController {
 		Stage window = (Stage) btnSalvar.getScene().getWindow();
 		Scene loginScene = new Scene(loginP);
 		loginController loginController = loader.getController();
-		
+
 		window.setScene(loginScene);
 		loginController.notifyCadastro(user, empresa);
 
@@ -202,9 +197,10 @@ public class cadastroEmpresaController {
 		return Date.from(data.atStartOfDay(zoneidDefault).toInstant());
 	}
 
-	
 	@FXML
 	public void pfChecked() {
+		txtCNPJ.clear();
+		
 		radioTipoJuridica.setSelected(false);
 
 		txtRG.setDisable(false);
@@ -212,11 +208,14 @@ public class cadastroEmpresaController {
 		txtIE.setDisable(true);
 
 		txtIM.setDisable(true);
+
 	}
 
 	@FXML
 	public void pjChecked() {
 
+		txtCNPJ.clear();
+		
 		txtIE.setDisable(false);
 
 		txtIM.setDisable(false);
@@ -281,6 +280,58 @@ public class cadastroEmpresaController {
 			comboCidade.getItems().add(cidades.getNome());
 
 		}
+	}
+
+	@FXML
+	private void formatCEP() {
+
+		TextFieldFormatter CEPmask = new TextFieldFormatter();
+		CEPmask.setMask("#####-###");
+		CEPmask.setCaracteresValidos("0123456789");
+		CEPmask.setTf(txtCEP);
+		CEPmask.formatter();
+
+	}
+
+	@FXML
+	private void formatCPFCPNJ() {
+		
+		if (radioTipoFisica.isSelected()) {
+			TextFieldFormatter CPFmask = new TextFieldFormatter();
+			CPFmask.setMask("###.###.###-##");
+			CPFmask.setCaracteresValidos("0123456789");
+			CPFmask.setTf(txtCNPJ);
+			CPFmask.formatter();
+
+		} else if (radioTipoJuridica.isSelected()){
+			
+			TextFieldFormatter CNPJmask = new TextFieldFormatter();
+			CNPJmask.setMask("##.###.###.###/####-##");
+			CNPJmask.setCaracteresValidos("0123456789");
+			CNPJmask.setTf(txtCNPJ);
+			CNPJmask.formatter();
+		}
+
+	}
+
+	@FXML
+	private void formatPhone() {
+		
+		TextFieldFormatter TelefoneMask = new TextFieldFormatter();
+		TelefoneMask.setMask("(##)#####-####");
+		TelefoneMask.setCaracteresValidos("0123456789");
+		TelefoneMask.setTf(txtTelefone);
+		TelefoneMask.formatter();
+	}
+
+	@FXML
+	private void formatRG() {
+		
+		TextFieldFormatter RGmask = new TextFieldFormatter();
+		RGmask.setMask("###########");
+		RGmask.setCaracteresValidos("0123456789");
+		RGmask.setTf(txtRG);
+		RGmask.formatter();
 	}
 
 }
