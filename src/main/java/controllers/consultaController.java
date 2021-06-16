0ee1ccd49f4ae643166667fbcd074059b364e214
andreyknsz <65321603+andreyknsz@users.noreply.github.com>
2021-06-15 22:27:09 -0300

    Development (#9)
    * Telas de filtros de Usuario e Veterinario
    
    * filtro de usuario e veterinario
    
    * senha persistence.xml
    
    * TODO fix Filtros
    
    * Fix filtro

--- src/main/java/controllers/consultaController.java
+++ src/main/java/controllers/consultaController.java
@@ -44,7 +44,7 @@ public class consultaController {
 	private Button btnFechar;
 
 	@FXML
-	private TableView2<Object> tableConsulta;
+	public TableView2<Object> tableConsulta;
 
 	@FXML
 	private SplitMenuButton menuConsulta;
@@ -69,7 +69,7 @@ public class consultaController {
 	}
 
 	public void setPerspectiveList(ObservableList<Object> perspectiveList) {
-		this.perspectiveList = perspectiveList;
+		this.perspectiveList = perspectiveList;	
 	}
 
 	public Usuarios getUser() {
@@ -183,15 +183,14 @@ public class consultaController {
 
 	@FXML
 	public void consultarUsuarios() {
-
 		setCurrentPerspective("Usuarios");
 		tableConsulta.getColumns().clear();
 		tableConsulta.getItems().clear();
 
-		setPerspectiveList(getUsuarios());
+//		setPerspectiveList(getUsuarios());
 
 		tableConsulta.setItems(getPerspectiveList());
-
+		System.out.println("1>" + getPerspectiveList().toString());
 		// Colunas
 		TableColumn<Object, String> nomeCol = new TableColumn<>("Nome");
 		nomeCol.setCellValueFactory(new PropertyValueFactory<>("nome"));
@@ -216,7 +215,7 @@ public class consultaController {
 			if (userMestre) {
 				resultado = "Sim";
 			} else {
-				resultado = "NÃ£o";
+				resultado = "Não";
 			}
 			return new ReadOnlyStringWrapper(resultado);
 		});
@@ -325,11 +324,7 @@ public class consultaController {
 			filtroStage.initModality(Modality.APPLICATION_MODAL);
 			filtroStage.setScene(filtroScene);
 			filtroStage.showAndWait();
-			
-			if (getPerspectiveList().isEmpty()) {
-				setPerspectiveList(getUsuarios());
-			}
-
+			consultarUsuarios();
 		}
 		if (perspectiva == "Veterinarios") {
 			fxmlFiltro = getClass().getResource("/fxml/FiltroDeVeterinario.fxml");
