0f07d50213f2280501c215a92e5e236ae4c412b7
Low Zaar <watchmefail12@gmail.com>
2021-11-10 22:53:13 -0300

    senhas omitidas

--- src/main/java/com/gadomanager/gadomanager/controllers/cadastroEmpresaController.java
+++ src/main/java/com/gadomanager/gadomanager/controllers/cadastroEmpresaController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import com.gadomanager.gadomanager.classes.Cidades;
