5a596756bc84b7c694851b6305f567b964b4943a
Low Zaar <watchmefail12@gmail.com>
2021-11-11 22:20:39 -0300

    projeto convertido para dependencias maven

--- module-info.java
+++ module-info.java
@@ -1,12 +1,9 @@
 module gadoManager {
 
-	requires javafx.base;
 	requires javafx.controls;
-	requires javafx.graphics;
 	requires javafx.fxml;
-	requires java.sql;
-	requires org.controlsfx.controls;
-	requires java.persistence;
-	opens gadoManager to javafx.graphics, javafx.controls;
-	exports gadoManager;
+	requires javafx.collections.ObservableList;
+	requires org.apache.poi.ooxml;
+	
+	opens com.gadomanager.gadomanager to javafx.fxml;
 }
